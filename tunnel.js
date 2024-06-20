6a8d614ba44d7d6a2b2a358b69652108ca787923
bangkarlsen <jhpe04@gmail.com>
2024-06-20 09:57:48 +0200

    Use better texture

--- tunnel.js
+++ tunnel.js
@@ -11,6 +11,8 @@
   const height = 176;
   const width = 320;
   let texture;
+  let dist = new Array();
+  let angle = new Array();
 
   const putPixel = (x, y, [r, g, b, a], chunky) => {
     const index = y * (width * 4) + x * 4;
@@ -20,13 +22,25 @@
     chunky[index + 3] = a;
   };
 
-  const drawTable = (time, chunky) => {
-    const offset = time % 100;
+  const drawScreen = (time, chunky) => {
+    //calculate the shift values out of the animation value
+    // int shiftX = int(texWidth * 1.0 * animation);
+    // int shiftY = int(texHeight * 0.25 * animation);
+    const shiftX = texture.width * 0.0001 * time;
+    const shiftY = Math.sin(0.001 * time) * 0.01;
+
     for (let x = 0; x < width; x++) {
       for (let y = 0; y < height; y++) {
         // const c = [(15 + y + offset) % 255, 35, 200 - x / 2, 255];
-        const index = y * (texture.width * 4) + x * 4;
-        const c = [texture.data[index], texture.data[index + 1], texture.data[index + 2], texture.data[index + 3]];
+        // const index = y * (texture.width * 4) + x * 4;
+        // int color = texture[(unsigned int)(distanceTable[y][x] + shiftX)  % texWidth][(unsigned int)(angleTable[y][x] + shiftY) % texHeight];
+        const texX = Math.floor(dist[y * width + x] + shiftX) % texture.width;
+        let texY = Math.floor(angle[y * width + x] + shiftY) % texture.height;
+        while (texY < 0) {
+          texY += texture.height;
+        }
+        const base = texY * (texture.width * 4) + texX * 4;
+        const c = [texture.data[base], texture.data[base + 1], texture.data[base + 2], 255];
         putPixel(x, y, c, chunky);
       }
     }
@@ -43,17 +57,47 @@
     if (!start) start = timestamp;
 
     const interval = timestamp - start;
-    if (interval > 600) {
+    if (interval > 0) {
       start = timestamp;
       draw(timestamp);
     }
     window.requestAnimationFrame(animate);
   };
 
+  const precalc = () => {
+    /*
+      //generate non-linear transformation table
+  for(int y = 0; y < h; y++)
+  for(int x = 0; x < w; x++)
+  {
+    int angle, distance;
+    float ratio = 32.0;
+    distance = int(ratio * texHeight / sqrt((x - w / 2.0) * (x - w / 2.0) + (y - h / 2.0) * (y - h / 2.0))) % texHeight;
+    angle = (unsigned int)(0.5 * texWidth * atan2(y - h / 2.0, x - w / 2.0) / 3.1416);
+    distanceTable[y][x] = distance;
+    angleTable[y][x] = angle;
+  }
+      * */
+    const ratio = 32;
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        // distance from this pixel to center of screen
+        const d =
+          ((ratio * texture.height) /
+            Math.sqrt((x - width / 2) * (x - width / 2) + (y - height / 2) * (y - height / 2))) %
+          texture.height;
+        // angle from this pixel to center of screen
+        const a = (0.5 * texture.width * Math.atan2(y - height / 2, x - width / 2)) / Math.PI;
+        dist[y * width + x] = d;
+        angle[y * width + x] = a;
+      }
+    }
+  };
+
   const init = () => {
     const img = new Image();
     img.crossOrigin = 'anonymous';
-    img.src = './texture.jpg';
+    img.src = './texture_small.jpg';
     img.addEventListener('load', () => {
       const buffer = document.createElement('canvas');
       const ctx = buffer.getContext('2d');
@@ -61,11 +105,11 @@
       buffer.height = img.height;
       ctx.drawImage(img, 0, 0, img.width, img.height);
       texture = ctx.getImageData(0, 0, buffer.width, buffer.height);
+      precalc();
       animate(0);
     });
   };
 
   // draw(0);
-  // animate(0);
   init();
 })();
